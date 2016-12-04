cde618b5085f69b1db95b517edb72fc1af1d5107
Sébastien Le Callonnec <sebastien@weblogism.com>
2016-12-04 11:53:43 +0000

    Listen to RabbitMQ queue.

--- README.md
+++ README.md
@@ -1,6 +1,28 @@
 Experimenting with OTP `gen_event`.
 
 
+## Build
+
+```
+rebar3 compile
+```
+
+## Run
+
+```
+erl -env ERL_LIBS _build/default/lib -eval 'application:ensure_all_started(durandaal).'
+```
+
+Sending `Pouet`:
+
+```
+1> Pid = <0.63.0>
+durandaal:pouet(pid(0,63,0)).
+Pouet !
+```
+
+Once `Pouet` is sent, durandaal listens on the `queue.duran` RabbitMQ queue.
+
 ## Why “Durandaal”?
 
 Because [Excaalibur](https://github.com/tychobrailleur/excaalibur).
--- src/durandaal.erl
+++ src/durandaal.erl
@@ -1,4 +1,5 @@
 -module(durandaal).
+-include_lib("amqp_client/include/amqp_client.hrl").
 -export([start_link/0, pouet/1]).
 
 start_link() ->
@@ -8,4 +9,32 @@ start_link() ->
     {ok, Pid}.
 
 pouet(Pid) ->
-    gen_event:notify(Pid, pouet).
+    gen_event:notify(Pid, pouet),
+    start_listening().
+
+
+start_listening() ->
+    {ok, Connection} = amqp_connection:start(#amqp_params_network{}),
+    {ok, Channel} = amqp_connection:open_channel(Connection),
+    Declare = #'queue.declare'{queue = <<"queue.duran">>, durable = true },
+    #'queue.declare_ok'{queue = Q} = amqp_channel:call(Channel, Declare),
+
+    Sub = #'basic.consume'{queue = Q},
+    #'basic.consume_ok'{} = amqp_channel:call(Channel, Sub),
+    listen_queue(Channel).
+
+
+listen_queue(Channel) ->
+    receive
+        #'basic.consume_ok'{} ->
+            listen_queue(Channel);
+
+        #'basic.cancel_ok'{} ->
+            ok;
+
+        {#'basic.deliver'{delivery_tag = Tag}, Content} ->
+            #'amqp_msg'{ payload = Payload } = Content,
+            io:format("Content = ~p.~n", [Payload]),
+            amqp_channel:cast(Channel, #'basic.ack'{delivery_tag = Tag}),
+            listen_queue(Channel)
+    end.
